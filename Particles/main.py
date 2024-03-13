f9d31b406ade4815b2dd0adb5cc56766ba1fed41
Muhammad Azeem Haider <mh06858@st.habib.edu.pk>
2024-03-14 02:15:37 +0500

    implementing GUI

--- Particles/main.py
+++ Particles/main.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 from raindrop import RainDrops
 from cloud import Clouds
+from GUI import GUI
 
 pygame.init()
 
@@ -18,33 +19,44 @@ flock = []
 PARTICLE_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(PARTICLE_EVENT, 40)  # After how many milliseconds will each event be triggered.
 
-individualraindrops = RainDrops(SCREEN_HEIGHT)
+individualraindrops = RainDrops()
 individualclouds = Clouds()
 
 mean_raindrops = 10
 var_raindrops = 5
 
 def main():
+    gui = GUI(SCREEN_WIDTH, SCREEN_HEIGHT)  # Create an instance of the GUI class
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == PARTICLE_EVENT:
-                individualraindrops.add(SCREEN_WIDTH ,mean_raindrops, var_raindrops)
-                # print("Adding raindrops")
+                individualraindrops.add(SIMULATION_AREA_WIDTH, mean_raindrops, var_raindrops)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     individualclouds.add(event.pos[0], event.pos[1])
-                    # print("Adding clouds")
 
+            # Pass events to the GUI for handling
+            gui.handle_event(event)
+
+        # Draw simulation area
         screen.fill((200, 200, 255))
 
         individualclouds.emit(screen)
         individualraindrops.emit(screen)
-        # print("Number of raindrops:", len(individualraindrops.raindrops))
+        pygame.display.flip()
+
+        # Draw control area (GUI)
+        pygame.draw.rect(screen, (150, 150, 150), (SIMULATION_AREA_WIDTH, 0, CONTROL_AREA_WIDTH, SCREEN_HEIGHT))
+
+        gui.draw(screen)  # Draw GUI elements
+
         pygame.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
--- Particles/raindrop.py
+++ Particles/raindrop.py
@@ -15,7 +15,7 @@ class RainDrops:
                 particle[0][1] += particle[2][0]  # Move the particle in the x direction
                 particle[0][0] += particle[2][1]  # Move the particle in the y direction
                 # particle[2][1] += 0.1  # Acceleration factor to the particle, this will be used to simulate the wind pressure
-                particle[1] -= 0.05  # Dampen the particle size, that is how fast the particle will shrink/die off
+                particle[1] -= 0.08  # Dampen the particle size, that is how fast the particle will shrink/die off
                 pygame.draw.circle(screen, pygame.Color('White'), particle[0], int(particle[1]))
 
     def add(self, SCREEN_WIDTH, mean_raindrops, var_raindrops):
@@ -23,7 +23,7 @@ class RainDrops:
         for _ in range(num_raindrops):
             pos_x = random.randint(0, SCREEN_WIDTH)
             pos_y = 0 
-            radius = 10
+            radius = 7.5
             direction_x = random.randint(1, 5)
             direction_y = 0  # Ensure raindrops move downwards
             particle_circle = [[pos_x, pos_y], radius, [direction_x, direction_y]]
